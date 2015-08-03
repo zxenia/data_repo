38c93d7b2d62d4a452e5787aab815297970091ae
zxenia <ksenia1zaytseva@gmail.com>
2015-08-03 13:38:08 +0200

    added test file for dbpedia spotlight testing

--- spotlighttest_03.py
+++ spotlighttest_03.py
@@ -3,11 +3,15 @@ __author__ = 'Ksenia'
 import spotlight
 import os, re
 
+#import pickle
+
 import xml.dom.minidom
 from xml.dom.minidom import Node
 from xml.dom.minidom import parse, parseString
 
-data_to_process = xml.dom.minidom.parse(r'C:\Users\Ksenia\Desktop\OEBL_XML-Daten\Aberle_Karl_1818_1892.xml')
+import io, json
+
+data_to_process = xml.dom.minidom.parse(r'C:\Users\kzaytseva\Documents\Repos\pyckathon\APIS-extraction\sample_data\OEBL_XML-Daten\Aberle_Karl_1818_1892.xml')
 #variable to process xml file
 
 topic = data_to_process.getElementsByTagName('Lexikonartikel')
@@ -21,10 +25,29 @@ for node in topic:
 
 annotations = spotlight.annotate('http://spotlight.dbpedia.org/rest/annotate', Value, confidence=0.2, support=20)
 
-myfile = open('haupttext_spot.html', 'w')
-for item in annotations:
-    myfile.write(str(item) + '\n')
-myfile.close()
+
+pi = json.dumps(annotations, sort_keys=True, indent=4, separators=(', ', ': '))
+#print(annotations)
+
+
+print(pi)
+
+pi = open('test_haupttext.html', 'w') #writing output in html file
+
+#out_string = str(annotations) #convering annotations value to string
+
+#myfile. write(out_string + '\n')
+
+#out = json.dump(annotations, myfile, sort_keys = True, indent = 4, )
+
+#myfile.write(out + '\n')
+
+pi.close()
+
+
+#for item in annotations:
+    #myfile.write(str(item) + '\n')
+#myfile.close()
 
 
 
